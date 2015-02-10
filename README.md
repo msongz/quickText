2a313056ca4d3f4183022f0675584a1f396dd65e
songz <msongz@qq.com>
2015-02-11 00:10:28 +0800

    初期添加

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# quickText
-make a subtitle in ae quickly
+
+# Make **subtitle** in AE quickly
+
+[![](http://ww1.sinaimg.cn/bmiddle/66e22e28jw1ene7pb7q0ig20c809xwod.gif)](http://weibo.com/songz)
+
+### Description
+
+  - localization `for English and Chinese`
+
+  - text layer the script created only `the functional text layer was applyed some expressions on the property of source text so the other text layers you add would not take effect. `
+
+  - markers `all your subtitle were depending on the marker's time.`
+
+  - style `as all subtitle on one layer,you could change font or color by once.` 
+
+### Usage & Installation
+
+  - put the jsx to :open_file_folder: `/ae installation folder/support file/script/`
+   
+  or
+
+  - put the jsx to :open_file_folder: `/ae installation folder/support file/script/ScriptUI Panels/`
+   
+    if you would like it dockable 
+
+    then
+
+  - run it from the <kbd>file</kbd>><kbd>scripts</kbd>><kbd>run script file</kbd>><kbd>stopMotion.jsx</kbd> or <kbd>window</kbd>>choose <kbd>stopMotion.jsx</kbd>
+
+### Compatibility 
+
+  after effects `cc 2014` `cc` `cs6` `cs5` `or older`
+
+### Version
+
+ - 5.0.1
+
+### Open source
+
+ - open source `These files are available under a sort of "open source" concept. They are intended for educational use but really can be used for whatever the fuck you want. You don't need to credit me for whatever commercial or non-commercial use you make of them, but if you could shout me a coffee with any project that do come from them, I'd love to drink it :)`
+
---
+++ quickText.jsx
@@ -0,0 +1,135 @@
+﻿//quick Text  
+//version 5.01
+//2015 01.02 23:20
+//copyright (c) songz meng
+
+//global
+
+qT_Data = new Object();
+qT_Data.scriptName = {
+    en: "quickText",
+    cn: "快速字幕"
+};
+qT_Data.layerName = {
+    en: "subtitle",
+    cn: "字幕"
+};
+qT_Data.inPuText = {
+    en: "input text",
+    cn: "输入字幕"
+}
+qT_Data.addText = {
+    en: "add text / marker",
+    cn: "添加字幕/空标记"
+};
+qT_Data.keepWord = {
+    en: "keep text",
+    cn: "保留文字"
+};
+qT_Data.addTlayer = {
+    en: "add T layer",
+    cn: "添加字层"
+};
+qT_Data.geText = {
+    en: "get text",
+    cn: "取字"
+};
+qT_Data.scriptTitle = {
+    en: "quickText v5",
+    cn: "快速字幕"
+};
+qT_Data.noComp = {
+    en: "Please select a single composition in the Project panel, and try again.",
+    cn: "在项目面板选中或打开一个合成"
+};
+qT_Data.noLayer = {
+    en: "Please select a layer, and try again.",
+    cn: "请选中一个图层"
+};
+qT_Data.noTextLayer = {
+    en: "not a T Layer",
+    cn: "非文字层"
+};
+qT_Data.error = {
+    en: "error",
+    cn: "错误"
+}
+qT_Data.about = {
+    en: "thank you for using quicktext ae script. \n " + "\n" + "contact me: weibo.com/songz",
+    cn: "谢谢使用\n" + "\n" + "联系我 weibo.com/songz"
+};
+qT_Data.nonSelect = {
+    en: "no layer was selected",
+    cn: "没有选中图层！"
+}
+
+//localize
+
+function qT_localize(Var) {
+    if (app.isoLanguage === "zh_CN") {
+        return Var["cn"];
+    } else {
+        return Var["en"];
+    }
+}
+
+//ui
+
+win = new Window('palette', qT_localize(qT_Data.scriptName));
+wP = win.add("panel", [0, 0, 225, 150], qT_localize(qT_Data.inPuText));
+wP.eT = wP.add("edittext", [15, 15, 205, 35]);
+wP.aM = wP.add("button", [15, 45, 205, 75], qT_localize(qT_Data.addText));
+wP.cK = wP.add("checkbox", [15, 85, 100, 105], qT_localize(qT_Data.keepWord));
+wP.qU = wP.add("button", [180, 85, 205, 100], '?');
+wP.aT = wP.add("button", [15, 110, 95, 130], qT_localize(qT_Data.addTlayer));
+wP.gT = wP.add("button", [125, 110, 205, 130], qT_localize(qT_Data.geText));
+
+//main function
+
+var aI = app.project.activeItem;
+
+wP.aT.onClick = function() {
+    if (!(aI instanceof CompItem)) {
+        alert(qT_localize(qT_Data.noComp), qT_localize(qT_Data.error)) //
+    } else {
+        app.beginUndoGroup(qT_localize(qT_Data.scriptName));
+        aI.layers.addText(qT_localize(qT_Data.layerName));
+        aI.layer(1).property("Marker").setValueAtTime(aI.layer(1).startTime, new MarkerValue(""));
+        aI.layer(1).sourceText.expression = "eval('@JSXBIN@ES@2.0@MyBbyBn0ABOAbAn0AHJAnABjzBjOBfXzFjJjOjEjFjYCfEXzKjOjFjBjSjFjTjUiLjFjZDfjzGjNjBjSjLjFjSEfRBjzEjUjJjNjFFfffnfJAnABjzBjUGfXFfEXzDjLjFjZHfjEfRBjBfffnfOAbAn0ABJAnAPjBfyBtACzBhcICzBhNJjFfjGfnnnndAnOAbAn0ABJAnABjBfndBfACIjBfnndBnJAnAjBfJAnABjzBjTKfXzHjDjPjNjNjFjOjULfEXHfjEfRBjBfffnfJAnAjKfACzBheMXzHjOjVjNiLjFjZjTNfjEfnndAn0DzAOByB')"
+        aI.layer(1).position.setValue([aI.width * 0.5, aI.height * 0.9]);
+        app.endUndoGroup();
+    }
+}
+
+wP.gT.onClick = function() {
+    if (!(aI instanceof CompItem) || aI.selectedLayers.length == 0) {
+        alert(qT_localize(qT_Data.noLayer), qT_localize(qT_Data.error))
+    } else if (aI.selectedLayers[0].property("sourceText") != null) {
+
+        wP.eT.text = aI.selectedLayers[0].sourceText.value
+    } else {
+        alert(qT_localize(qT_Data.noTextLayer), qT_localize(qT_Data.error), "x")
+    }
+}
+
+wP.aM.onClick = function() {
+    if (!(aI instanceof CompItem) || aI.selectedLayers.length == 0) {
+        alert(qT_localize(qT_Data.nonSelect), qT_localize(qT_Data.error), "x");
+    } else {
+        a = wP.eT.text
+        mK = new MarkerValue(a);
+        sL = aI.selectedLayers[0];
+        CTI = aI.time;
+        sL.property("Marker").setValueAtTime(CTI, mK);
+        if (!(wP.cK.value)) {
+            wP.eT.text = ""
+
+        }
+    }
+};
+
+wP.qU.onClick = function() {
+    alert(qT_localize(qT_Data.about), qT_localize(qT_Data.scriptTitle))
+};
+
+win.show();
